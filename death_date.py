086858f255a4cdd98dabb7db0d0a0bb6631625f5
Noah-Jade <108606203+Noah-Jade@users.noreply.github.com>
2022-07-03 06:29:26 -0500

    Add files via upload

---
+++ death_date.py
@@ -0,0 +1,24 @@
+from random import *
+from datetime import date
+
+
+name = input("What is your name? ")
+birth = input("What year were you born? ")
+question = input("Would you like to know when you'll die? Enter Yes or No... ")
+
+today = date.today()
+
+
+def death_date():
+    return randint(1, 60) + today.year
+
+
+if question == "Yes":
+    print("You will perish in " + str(death_date()) + ", " + name + ". Rip :(.")
+else:
+    print("Your fate remains a mystery " + name + ".")
+
+
+# def age():
+#    return today.year - int(birth)
+
